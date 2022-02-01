6fdfc0d2aaea58cf9e408d1b23188e70198a8dd0
Andrew Nordlund <github@nordburg.ca>
2022-02-01 13:57:55 -0500

    Added some language switching content

--- langTest/langTest.js
+++ langTest/langTest.js
@@ -1,11 +1,19 @@
-let langSel = null;
+let langSel, p1 = null;
 
 langSel = document.getElementById("langSel");
+p1 = document.getElementById("p1");
+
 langSel.addEventListener("change", toggleLang, false);
 
 function toggleLang (e) {
 	console.log ("Toggling language...");
 	console.log ("Language: " + document.documentElement.lang + ".");
-	document.documentElement.lang = (document.documentElement.lang === "en" ? "fr" : "en");
+	if (document.documentElement.lang === "en") {
+		document.documentElement.lang = "fr";
+		p1.textContent = "Ceci est le premier paragraphe.";
+	} else {
+		document.documentElement.lang = "en";
+		p1.textContent = "This is the first paragraph.";
+	}
 	console.log ("Language: " + document.documentElement.lang + ".");
 } // End of toggleLang
