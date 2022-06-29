578cfc409af7a8ac99a7dd354728f74639261805
Andrew Nordlund <github@nordburg.ca>
2022-06-29 17:56:04 -0400

    Adding to font test

--- fontTest/fontTest.js
+++ fontTest/fontTest.js
@@ -1,7 +1,20 @@
 let fontOL = document.getElementById("fontOL");
+let fontFams = [
+"Arial",
+"Verdana",
+"Helvetica",
+"Tahoma",
+"Trebuchet MS",
+"Times New Roman",
+"Georgia",
+"Garamond",
+"Courier New",
+"Brush Script MT"
+];
 
-for (let i = 9984; i <= 10020; i++) {
+for (let i = 0; i < fontFams.length; i++) {
 	let newLI = document.createElement("li");
-	newLI.textContent = i + ": " + String.fromCharCode(i);
+	newLI.setAttribute("style", "font-family: " + fontFams[i]);
+	newLI.textContent = fontFams[i] + ": ✓"; //i + ": " + String.fromCharCode(i);
 	fontOL.appendChild(newLI);
 }
