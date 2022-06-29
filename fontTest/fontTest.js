75b927d163d7643a2ffdb06d06ec57d7ed9e7dc6
Andrew Nordlund <github@nordburg.ca>
2022-06-29 17:59:48 -0400

    Adding to font test

--- fontTest/fontTest.js
+++ fontTest/fontTest.js
@@ -6,10 +6,21 @@ let fontFams = [
 "Tahoma",
 "Trebuchet MS",
 "Times New Roman",
+"Times",
 "Georgia",
+"Geneva",
 "Garamond",
 "Courier New",
-"Brush Script MT"
+"Brush Script MT",
+"cursive",
+"Courier New",
+"Courier",
+"Coperplate",
+"Papyrus",
+"fantasy",
+"monospace",
+"serif",
+"sans-serif",
 ];
 
 for (let i = 0; i < fontFams.length; i++) {
