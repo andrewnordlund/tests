77f35917a9337c40b5a654fc4023acf3e8705158
Andrew Nordlund <github@nordburg.ca>
2025-03-07 18:08:08 -0500

    Updated 1280px stuff in sizeTest

--- sizeTest/1280px.js
+++ sizeTest/1280px.js
@@ -1,7 +1,8 @@
-let sizeDiv = null;
+let sizeDiv, main = null;
 //let resizeBtn = null;
 let theWidth = 0;
 
+main = document.getElementsByTagName("main")[0];
 sizeDiv = document.getElementById("sizeDiv");
 //resizeBtn = document.getElementById("resizeBtn");
 
@@ -14,6 +15,13 @@ if (sizeDiv) {
 
 function setWidthText () {
 	sizeDiv.innerHTML = window.innerWidth + "px";
+	if (window.innerWidth == 1280) {
+		main.classList.add("rightSize");
+		sizeDiv.innerHTML += " Ready for Reflow"
+		console.log ("Adding rightSize class.");
+	} else {
+		main.classList.remove("rightSize");
+	}
 } // End of setWidthText
 
 /*
